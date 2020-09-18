17a8543d8ff3bd63345602a03edc59b719f4f8b6
Ralph <ghannamralph@gmail.com>
2020-09-18 00:15:04 -0400

    Updates

---
+++ precache-manifest.0c5619a2699e8691f9a6475b5bd9c65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98365b18fdc4b9b55bd71d9fa95df669",
+    "url": "/Conways-Game-of-Life/index.html"
+  },
+  {
+    "revision": "a58a2dccbec1dbd61321",
+    "url": "/Conways-Game-of-Life/static/css/main.e7b69e6b.chunk.css"
+  },
+  {
+    "revision": "911f6a664ae4d5b35724",
+    "url": "/Conways-Game-of-Life/static/js/2.51ce98f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Conways-Game-of-Life/static/js/2.51ce98f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58a2dccbec1dbd61321",
+    "url": "/Conways-Game-of-Life/static/js/main.008aa2a2.chunk.js"
+  },
+  {
+    "revision": "6051ee7012578724c759",
+    "url": "/Conways-Game-of-Life/static/js/runtime-main.60924b30.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Conways-Game-of-Life/precache-manifest.0c5619a2699e8691f9a6475b5bd9c65b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Conways-Game-of-Life/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
